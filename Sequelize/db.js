e577335466c80a3e9a987703d322c2cc24863f96
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-08-01 16:41:33 -0300

    db

--- Sequelize/db.js
+++ Sequelize/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('crud', 'root', 'Sa05413196*',{
+const sequelize = new Sequelize('crud', 'root', 'Senha',{
     dialect: 'mysql',
     host: 'localhost',
     port: 3306
